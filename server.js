b1adea5c2bf5c47bbc43a8f80f4cbd0697b4868d
Connor James Leech <connorleech@gmail.com>
2013-12-23 13:26:39 +0300

    complete todo api

--- server.js
+++ server.js
@@ -21,5 +21,57 @@ app.configure(function(){
     app.use(express.methodOverride());
 });
 
+// define model
+var Todo = mongoose.model('Todo', {
+	text: String
+	// MongoDB will automatically generate an _id
+});
+
+// ================ routes ====================
+
+// get all todos
+app.get('/api/todos', function(req, res){
+	// mongoose gets all todos in the database
+	Todo.find(function(err, todos){
+		if (err) res.send(err);
+		
+		res.json(todos); // return in json format
+	});
+});
+
+// create todo
+app.post('/api/todos', function(req, res){
+	Todo.create({
+		// info will come from angular ajax request
+		text: req.body.text,
+		done: false
+	}, function(err, todo){
+		if(err) res.send(err);
+		// return all todos
+		Todo.find(function(err, todos){
+			if(err) res.send(err);
+			
+			res.json(todos);
+		});
+	});
+});
+
+// delete a todo
+app.delete('/api/todos/:todos_id', function(req, res){
+	Todo.remove({
+		_id: req.params.todo_id
+	}, function(err, todo){
+		if (err) res.send(err);
+		
+		// return all todos
+		Todo.find(function(err, todos){
+			if (err) res.send(err);
+			
+			res.json(todos);
+		});
+	});
+});
+
+// listen
 app.listen(8080);
 console.log('app is listening on port 8080');
